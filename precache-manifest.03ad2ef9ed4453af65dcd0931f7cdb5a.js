35fc199a0e468cb029366abf0302218ee84161d3
int <yuskovss@gmail.com>
2019-11-03 00:26:44 +0400

    Updates

---
+++ precache-manifest.03ad2ef9ed4453af65dcd0931f7cdb5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "093deb2b6ab69d49720fe9c30ebe4d45",
+    "url": "/openweather/index.html"
+  },
+  {
+    "revision": "f3453b9ee207a1da5637",
+    "url": "/openweather/static/css/main.f3845c1d.chunk.css"
+  },
+  {
+    "revision": "2c2bc55c1f6e1a2034ba",
+    "url": "/openweather/static/js/2.9d69f40c.chunk.js"
+  },
+  {
+    "revision": "f3453b9ee207a1da5637",
+    "url": "/openweather/static/js/main.10cb3bf6.chunk.js"
+  },
+  {
+    "revision": "5a22eeecffe7fec8c427",
+    "url": "/openweather/static/js/runtime-main.0cc0f9ce.js"
+  }
+]);
